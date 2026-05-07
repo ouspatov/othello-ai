67d11bb985d1418f07e17cc47db145290fc16ccd
Orkhan <orkhanusp@gmail.com>
2026-05-07 18:16:12 +0200

    Initial Folder Structure

---
+++ Views/StartScreenView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace OthelloAI;
+
+public partial class StartScreenView : Window
+{
+    public StartScreenView()
+    {
+        InitializeComponent();
+    }
+}
